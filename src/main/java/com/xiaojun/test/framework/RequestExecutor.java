e31752928845ef81a02b5256b987cd174bb5e925
yongche <xiaojun@yongche.com>
2017-06-06 15:10:07 +0800

    test ng sample

--- src/main/java/com/xiaojun/test/framework/RequestExecutor.java
+++ src/main/java/com/xiaojun/test/framework/RequestExecutor.java
@@ -1,10 +1,11 @@
 package com.xiaojun.test.framework;
 
-import org.slf4j.*;
-
 /**
  * Created by yongche on 17/6/1.
  */
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 public class RequestExecutor {
     public static Logger log = LoggerFactory.getLogger(RequestExecutor.class);
 
---
+++ src/main/java/com/xiaojun/test/testcase/TestCaseDemo.java
@@ -0,0 +1,26 @@
+package com.xiaojun.test.testcase;
+
+/**
+ * Created by yongche on 17/6/6.
+ */
+
+import com.xiaojun.test.framework.core.*;
+import com.xiaojun.test.framework.*;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.testng.annotations.*;
+import org.testng.Assert;
+import org.testng.ITestResult;
+
+import java.util.*;
+
+public class TestCaseDemo {
+    public static Logger log = LoggerFactory.getLogger(TestCaseDemo.class);
+
+    @Test
+    public void load_data_sample() {
+        Map<String, TestParameter> inputParam = RequestDataFactory.getInputParameterFromXML("request_data_sample.xml", "test_case_sample");
+
+        Assert.assertEquals(inputParam.get("user_id").getValue(), "26016450");
+    }
+}
