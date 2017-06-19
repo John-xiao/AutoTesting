a75d90cd9be32afffd720dd8efd5d5ab049ecd1d
yongche <xiaojun@yongche.com>
2017-06-19 16:36:44 +0800

    add fastjson

--- src/main/java/com/xiaojun/test/framework/RequestExecutor.java
+++ src/main/java/com/xiaojun/test/framework/RequestExecutor.java
@@ -3,11 +3,14 @@ package com.xiaojun.test.framework;
 /**
  * Created by yongche on 17/6/1.
  */
+
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
 public class RequestExecutor {
     public static Logger log = LoggerFactory.getLogger(RequestExecutor.class);
 
+    public static excuteRequest(String requestDataFilePath, String caseName) {
 
+    }
 }
